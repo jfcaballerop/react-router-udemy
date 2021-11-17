79e36c76a42f9bc42c5803c1a9902330918d0af1
jfcaballerop <jfcaballerop@gmail.com>
2021-11-17 21:01:59 +0100

    add routes

--- src/pages/SearchScreen.jsx
+++ src/pages/SearchScreen.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router';
+import queryString from 'query-string'
+import { Characters } from '../models/Characters';
+import Card from '../components/Card';
 
-const SearchScreen = () => {
+const SearchScreen = ({ history }) => {
+	const location = useLocation();
+	const { q = "" } = queryString.parse(location.search)
+	const [inputValue, setinputValue] = useState(q);
+	const [characters, setcharacters] = useState([])
+	const handleChange = (e) => {
+		const val = e.target.value
+		setinputValue(val)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		history.push(`?q=${inputValue}`)
+	}
+
+	const getCharacters = () => {
+		if (inputValue.trim() !== "") {
+			const value = inputValue.toLocaleLowerCase()
+			const characters = Characters.filter(ch => ch.name.toLocaleLowerCase().includes(value))
+			setcharacters(characters)
+		} else {
+			setcharacters([])
+		}
+	}
+
+	useEffect(() => {
+		getCharacters();
+	}, [q])
+
 	return (
-		<div>
-			Search
+		<div className="container">
+			<h1> Search Screen</h1>
+			<hr />
+			<div className="row">
+				<div className="col-6">
+					<h2>
+						Search
+					</h2>
+					<form onSubmit={handleSubmit}>
+						<label className="form-label w-100"> Character:
+							<input type="text" placeholder="name" className="form-control" autoComplete="off" value={inputValue} onChange={handleChange} />
+						</label>
+						<button type="submit" className="btn btn-info w-100">Search</button>
+					</form>
+				</div>
+
+				<div className="col-6">
+					<h2>Results: {characters.length}</h2>
+					{
+						characters.length === 0 && (<div className="alert alert-warning text-center">Please search a character</div>)
+					}
+					{
+						characters.map(c => <Card key={c.id} {...c}></Card>)
+					}
+				</div>
+
+			</div>
+
 		</div>
 	)
 }
