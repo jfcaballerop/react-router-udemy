27377886123daeb04488d9b8f77d6eb86eb05db9
jfcaballerop <jfcaballerop@gmail.com>
2021-11-17 21:51:26 +0100

    end routes

--- src/routes/LoginRouter.jsx
+++ src/routes/LoginRouter.jsx
@@ -1,8 +1,7 @@
 import React, { useContext } from 'react'
 import {
 	BrowserRouter as Router,
-	Switch,
-	Route
+	Switch
 } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
 import Login from '../pages/Login'
